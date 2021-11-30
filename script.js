9ddf47d8d14f4519d8911e2c158cc2cb63a21564
Ruban Prakash <rubanprakash@hotmail.com>
2021-11-30 13:09:10 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+import { localQuotes } from "./quotes.js";
+
+// Helper Functions
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function element(id) {
+  return document.querySelectorAll(id)[0];
+}
+
+// Get Quotes From API
+async function getQuotes() {
+  const apiURL = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiURL);
+    const quotes = await response.json();
+    return quotes;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Update Page With New Quote From API
+function newQuoteAPI() {
+  getQuotes().then((quotes) => {
+    let randomQuote = quotes[getRandomInt(0, quotes.length - 1)];
+    element("#quote").innerText = randomQuote["text"];
+    element("#author").innerText = randomQuote["author"];
+    if (!element("#author")) {
+      element("#author").innerText = "Anon";
+    }
+  });
+}
+
+function newQuoteLocal() {
+  let randomQuote = localQuotes[getRandomInt(0, localQuotes.length - 1)];
+  element("#quote").innerText = randomQuote["text"];
+  if (randomQuote["author"] === null) {
+    element("#author").innerText = "Anon";
+  } else {
+    element("#author").innerText = randomQuote["author"];
+  }
+}
+
+// Generate New Quote on Clicking 'New Quote' Button || Pressing Space Bar
+document
+  .querySelectorAll("#newquote-button")[0]
+  .addEventListener("click", newQuoteAPI);
+
+document.addEventListener("keyup", (event) => {
+  if (event.which === 32) {
+    setTimeout(() => {
+      newQuoteAPI();
+    }, 1);
+  }
+});
