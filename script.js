a491098825e846e16d9c89e3c7433b1f02d2d3d7
Ruban Prakash <rubanprakash@hotmail.com>
2021-11-30 15:27:41 +0000

    functioning buttons

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 import { localQuotes } from "./quotes.js";
 
+let quote = document.getElementById("quote-text");
+let author = document.getElementById("author-text");
+
 // Helper Functions
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function element(id) {
-  return document.querySelectorAll(id)[0];
 }
 
 // Get Quotes From API
@@ -27,34 +26,43 @@ async function getQuotes() {
 function newQuoteAPI() {
   getQuotes().then((quotes) => {
     let randomQuote = quotes[getRandomInt(0, quotes.length - 1)];
-    element("#quote-text").innerText = randomQuote["text"];
+    quote.innerText = randomQuote["text"];
     if (randomQuote["author"] === null) {
-      element("#author-text").innerText = "Anon";
+      author.innerText = "Anon";
     } else {
-      element("#author-text").innerText = randomQuote["author"];
+      author.innerText = randomQuote["author"];
     }
   });
 }
 
 function newQuoteLocal() {
   let randomQuote = localQuotes[getRandomInt(0, localQuotes.length - 1)];
-  element("#quote-text").innerText = randomQuote["text"];
+  quote.innerText = randomQuote["text"];
   if (randomQuote["author"] === null) {
-    element("#author-text").innerText = "Anon";
+    author.innerText = "Anon";
   } else {
-    element("#author-text").innerText = randomQuote["author"];
+    author.innerText = randomQuote["author"];
   }
 }
 
-// Generate New Quote on Clicking 'New Quote' Button || Pressing Space Bar
+// Generate New Quote on Clicking 'New Quote' Button
 document
   .querySelectorAll("#newquote-button")[0]
   .addEventListener("click", newQuoteAPI);
 
-document.addEventListener("keyup", (event) => {
-  if (event.which === 32) {
-    setTimeout(() => {
-      newQuoteAPI();
-    }, 1);
-  }
-});
+// Share Quote To Twitter
+function tweetQuote() {
+  const twitterURL = `https://twitter.com/intent/tweet?text=${quote.innerText} - ${author.innerText}`;
+  window.open(twitterURL, "_blank");
+}
+
+function searchWikipedia() {
+  const wikipediaURL = `https://en.wikipedia.org/wiki/${author.innerText}`;
+  window.open(wikipediaURL, "_blank");
+}
+
+document.getElementById("twitter-button").addEventListener("click", tweetQuote);
+
+document
+  .getElementById("wiki-search-button")
+  .addEventListener("click", searchWikipedia);
